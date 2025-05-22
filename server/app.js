66d86713505d0385b22ed16ccc54dd71341bd423
Harsha <97773381+Harsha-182@users.noreply.github.com>
2025-05-22 15:13:15 +0530

    Resolve Function

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 
 const app = express();
-const schema = require('./schema/schema');
+const schema = require('./schema');
 
 app.use("/graphql", graphqlHTTP({
-
+	schema,
+	graphiql: true
 }));
 
 app.listen(5000, () => {
--- server/schema.js
+++ server/schema.js
@@ -1,5 +1,18 @@
-const schema = require('graphql');
-const { GraphQLObjectType, GraphQLString } = graphql;
+const graphql = require('graphql');
+const _= require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema } = graphql;
+
+const book = [
+	{id: '1', name: 'Money-Heist', genre: 'Thriller'},
+	{id: '2', name: 'Breaking Bad', genre: 'Drama'},
+	{id: '3', name: 'Game of Thrones', genre: 'Fantasy'},
+	{id: '4', name: 'The Witcher', genre: 'Fantasy'},
+	{id: '5', name: 'The Office', genre: 'Comedy'},
+	{id: '6', name: 'Stranger Things', genre: 'Sci-Fi'},
+	{id: '7', name: 'The Crown', genre: 'Drama'},
+	{id: '8', name: 'The Mandalorian', genre: 'Sci-Fi'},
+]
 
 const BookType = new GraphQLObjectType({
 	name: 'Book',
@@ -8,4 +21,23 @@ const BookType = new GraphQLObjectType({
 		name: {type: GraphQLString},
 		genre: {type: GraphQLString},
 	})
+});
+
+const RootQuery = new  GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		book: {
+			type: BookType,
+			args: {id: {type: GraphQLID}},
+			resolve(parent, args){
+				console.log("args:===========", args);
+				//code to get data from db /other source
+				return _.find(book, {id: args.id});
+			}
+		}
+	}
 })
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
+});
