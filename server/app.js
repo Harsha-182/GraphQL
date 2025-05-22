afa1b524d26b519b4432d6a8b86823cdccc39b8c
Harsha <97773381+Harsha-182@users.noreply.github.com>
2025-05-22 12:40:20 +0530

    Root Query

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
 const app = express();
+const schema = require('./schema/schema');
 
 app.use("/graphql", graphqlHTTP({
-	
+
 }));
 
 app.listen(5000, () => {
